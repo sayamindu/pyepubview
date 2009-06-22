9a1c9ee1a84fc3165eda9c38e02cf7e954658364
sayamindu <sayamindu@9c40bebc-5f36-11de-bbaa-f1a44a2b9d47>
2009-06-22 17:47:41 +0000

    Initial files

---
+++ epubview/setup.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python
+
+from distutils.core import setup
+
+setup(name='PyEpubView',
+      version='0.2',
+      description='Epub viewer PyGTK widget',
+      author='Sayamindu Dasgupta',
+      author_email='sayamindu@laptop.org',
+      url='http://code.google.com/p/pyepubview/',
+      packages=['epubview'],
+     )
